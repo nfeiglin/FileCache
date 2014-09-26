c80d8b07c17740eaaba8e362cb187f8c65a9d5d0
Nathan Feiglin <nathan@technewsu.com.au>
2014-09-26 23:45:46 +1000

    Starts adding file / cache manage class http://nshipster.com/nsfilemanager/

---
+++ FileCache.m
@@ -0,0 +1,13 @@
+//
+//  FileCache.m
+//  sailr
+//
+//  Created by Nathan Feiglin on 26/09/2014.
+//  Copyright (c) 2014 Sailr. All rights reserved.
+//
+
+#import "FileCache.h"
+
+@implementation FileCache
+
+@end
---
+++ JsonCache.m
@@ -0,0 +1,13 @@
+//
+//  JsonCache.m
+//  sailr
+//
+//  Created by Nathan Feiglin on 26/09/2014.
+//  Copyright (c) 2014 Sailr. All rights reserved.
+//
+
+#import "JsonCache.h"
+
+@implementation JsonCache
+
+@end
